1b25ab54cb369fc403fa5793ea83f64cc1dd1962
shassan <shassan@salesforce.com>
2019-02-23 16:52:50 +0530

    Bug fix in common_reduce ``` The map object initially was created for each intermidiate file and sorted and then written to output file
    But the output file should have the key sorted after reading from all
    the input file
    So map creation and sorting should be done outside the loop
    ```

--- src/mapreduce/common_reduce.go
+++ src/mapreduce/common_reduce.go
@@ -53,9 +53,10 @@ func doReduce(
 	// Your code here (Part I).
 	//
 
-	f, _ := os.Create(outFile)
+	f, _ := os.OpenFile(outFile, os.O_CREATE|os.O_APPEND|os.O_WRONLY, 0600)
+	enc := json.NewEncoder(f)
+	m := make(map[string][]string)
 	for i:=0 ; i < nMap ; i++ {
-		m := make(map[string][]string)
 		file := reduceName(jobName, i , reduceTask)
 		fileReader, _ := os.Open(file)
 		dec := json.NewDecoder(fileReader)
@@ -71,20 +72,18 @@ func doReduce(
 			m[kv.Key] = append(m[kv.Key], kv.Value)
 		}
 		fileReader.Close()
+	}
 
-
-		//iterate over the keys of map in sorted way.
-		//call the reduce function(key string, values []string) string
-		//The string returned by reduce function needs to be encoded and written in the output file
-		var keys []string
-		for k := range m {
-			keys = append(keys, k)
-		}
-		sort.Strings(keys)
-		enc := json.NewEncoder(f)
-		for _, k := range keys {
-			enc.Encode(KeyValue{k, reduceF(k, m[k])})
-		}
+	//iterate over the keys of map in sorted way.
+	//call the reduce function(key string, values []string) string
+	//The string returned by reduce function needs to be encoded and written in the output file
+	var keys []string
+	for k := range m {
+		keys = append(keys, k)
+	}
+	sort.Strings(keys)
+	for _, k := range keys {
+		enc.Encode(KeyValue{k, reduceF(k, m[k])})
 	}
 	f.Close()
 
